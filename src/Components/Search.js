c306afe98cae0dd1c9ad9c4622ec09f2728f76ac
elliot <elliotgcraig@icloud.com>
2019-01-12 12:34:38 -0700

    Search i think

---
+++ src/Components/Search.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            names: ['Liam', 'Noah', 'William', 'James', 'Logan', 'Benjamin', 'Mason', 'Elijah', 'Oliver', 'Jacob', 'Lucas', 'Michael', 'Alexander', 'Ethan', 'Daniel', 'Matthew', 'Aiden', 'Henry', 'Joseph', 'Jackson', 'Samuel', 'Sebastian', 'David', 'Carter', 'Wyatt', 'Jayden', 'John', 'Owen', 'Dylan', 'Luke', 'Gabriel', 'Anthony', 'Isaac', 'Grayson', 'Jack', 'Julian', 'Levi', 'Christopher', 'Joshua', 'Andrew', 'Lincoln', 'Mateo', 'Ryan', 'Jaxon', 'Nathan', 'Aaron', 'Isaiah', 'Thomas', 'Charles', 'Caleb', 'Josiah', 'Christian', 'Hunter', 'Eli', 'Jonathan', 'Connor', 'Landon', 'Adrian', 'Asher', 'Cameron', 'Leo', 'Theodore', 'Jeremiah', 'Hudson', 'Robert', 'Easton', 'Nolan', 'Nicholas', 'Ezra', 'Colton', 'Angel', 'Brayden', 'Jordan','Dominic', 'Austin', 'Ian'],
+            filteredNames: [],
+            searchTerm: '',
+        }
+
+    }
+    
+    updateSearch(text){
+        this.setState({
+            searchTerm: text,
+        })
+    }
+
+    render(){
+        
+        let filteredTiles = this.state.names.filter((name)=>{
+            const {searchTerm}=this.state
+            let searchLen = searchTerm.length
+            if(searchTerm.toUpperCase()===name.slice(0, searchLen).toUpperCase()){
+                return name
+            }
+        })
+        
+        let nameTiles = filteredTiles.map((el, i)=>{
+            return (
+                <div key={i}>
+                    <h3>{el}</h3>
+                </div>
+            )
+        })
+
+        
+       
+        return(
+            <div>
+                <input onChange={(e)=>this.updateSearch(e.target.value)} />
+                {nameTiles}
+            </div>
+        )
+    }
+}
+
+export default Search
